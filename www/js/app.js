154b3bef562a4425124161ec4cd2bc4c06deba9c
kmontenegro_ssf <kmontenegro@softstackfactory.org>
2015-08-12 22:01:40 +0000

    tk3

---
+++ www/js/app.js
@@ -0,0 +1,63 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'starter.controllers', 'RESTConnection', 'TKServicesModule'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+  .state('landing', {
+    url: '/',
+    templateUrl: 'templates/landing.html',
+  })
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller:'LoginCtrl'
+  })
+  .state('register', {
+    url: '/register',
+    templateUrl: 'templates/register.html',
+    controller:'RegisterCtrl'
+  })
+  .state('lobby', {
+    url: '/lobby',
+    templateUrl: 'templates/lobby.html',
+    controller:'LobbyCtrl'
+  })
+  .state('test', {
+    abstract: true,
+    url: '/test',
+    template: '<ion-nav-view></ion-nav-view>'
+  })
+  .state('test.detail', {
+    url: '/question:testID',
+    templateUrl: 'templates/question.html',
+    controller: 'TestCtrl',
+    resolve: {
+      testInfo: function($stateParams, TKQuestionsService) {
+        return TKQuestionsService.getQuestion($stateParams.testID);
+      }
+    }
+  })
+  .state('results', {
+    url: '/results',
+    templateUrl: 'templates/results.html'
+});
+
+})
+
---
+++ www/js/controller.js
@@ -0,0 +1,206 @@
+angular.module('starter.controllers', [ ])
+
+.controller('LoginCtrl', [ '$scope', '$state', 'UserService', '$ionicHistory', '$window',
+    function($scope, $state, UserService, $ionicHistory, $window) {
+    $scope.user = {};
+
+    $scope.loginSubmitForm = function(form)
+    {
+        if(form.$valid)
+        {   UserService.login($scope.user)
+            .then(function(response) {
+                if (response.status === 200) {
+                    $window.localStorage["userID"] = response.data.userId;
+                    $window.localStorage['token'] = response.data.id;
+                    alert("it got past window.localstorage");
+                    $ionicHistory.nextViewOptions({
+                      historyRoot: true,
+                      disableBack: true
+                    });
+                   $state.go('lobby');
+                    alert("I got past $state.go");
+                } else {
+                    // invalid response
+                    alert(response.status+ " Yes Something went wrong, try again.");
+                }
+            }, function(response) {
+                // Code 401 corresponds to Unauthorized access, in this case, the email/password combination was incorrect.
+                if(response.status === 401)
+                {
+                    alert("Incorrect username or password");
+                }else if(response.data === null) {
+//If the data is null, it means there is no internet connection. 
+                    alert("The connection with the server was unsuccessful, check your internet connection and try again later.");
+                }else {
+                    alert(response.data+ " Yes Something went wrong, try again.");
+                }
+
+            });
+
+            
+        }
+    };
+}])
+.controller('RegisterCtrl',['$scope', '$state', 'UserService', '$ionicHistory','$window', 
+    function($scope, $state, UserService, $ionicHistory, $window){
+        $scope.reg={};
+        $scope.repeatPassword= {};
+        alert("it got here");
+            //Required to get the access token
+   
+   
+    function loginAfterRegister()
+    {
+        UserService.login($scope.reg)
+        .then(function(response) {
+            if (response.status === 200) {
+                //Should return a token
+                $window.localStorage["userID"] = response.data.userId;
+                $window.localStorage['token'] = response.data.id;
+                $ionicHistory.nextViewOptions({
+                   historyRoot: true,
+                   disableBack: true
+                });
+                $state.go('lobby');
+            } else {
+                // invalid response
+                $state.go('landing');
+            }
+        }, function(response) {
+            // something went wrong
+            console.log(response);
+            $state.go('landing');
+        });
+    }
+    
+    
+        $scope.registerSubmitForm=function(form)
+        {
+            alert(JSON.stringify($scope.reg)+"it got in the function");
+            if(form.$valid){
+                 UserService.create($scope.reg)
+            .then(function(response) {
+                if (response.status === 200) {
+                   loginAfterRegister();
+                } else {
+                    // invalid response
+                    console.log(response);
+                    alert(response.status+ " Yes Something went wrong, try again.");
+                }
+            }, function(response) {
+                // Code 401 corresponds to Unauthorized access, in this case, the email/password combination was incorrect.
+                if(response.status === 422)
+                {
+                    alert("Email is already registered to an Account");
+                }else if(response.data === null) {
+//If the data is null, it means there is no internet connection. 
+                    alert("The connection with the server was unsuccessful, check your internet connection and try again later.");
+                }else {
+                    alert(response.data+ " Yes Something went wrong, try again. hi");
+                }
+
+            });
+            }
+            else{ alert("invalid");}
+        }
+        
+    }])
+    
+.controller('LobbyCtrl',['$scope', '$state', '$ionicHistory', 'UserService', '$window', 
+'ServerQuestionService', 'TKQuestionsService',
+function($scope, $state, $ionicHistory, UserService, $window,
+ServerQuestionService, TKQuestionsService) {
+    $scope.logout = function()
+    {
+         UserService.logout($window.localStorage.token)
+         .then(function(response) {
+            //The successful code for logout is 204
+            if(response.status === 204)
+            {
+                alert("it is a 204")
+                $ionicHistory.nextViewOptions({
+                  historyRoot: true,
+                  disableBack: true
+                });
+                $state.go('landing');
+            }else {
+                 alert("Could not logout at this moment, try again.");
+            }
+        }, function(response) {
+            alert("Could not logout at this moment, try again.");
+        });
+    }
+    //Get Questions Initially if they are not already stored
+    
+   
+    function getQuestions()
+    {
+        ServerQuestionService.all($window.localStorage['token'])
+        .then(function(response) {
+            if (response.status === 200) {
+                var questions = response.data;
+                TKQuestionsService.setQuestions(questions);
+                $window.localStorage.questions = JSON.stringify(questions);
+            } else {
+                // invalid response
+                confirmPrompt();
+            }
+        }, function(response) {
+            // something went wrong
+            confirmPrompt();
+        });
+    }
+    
+    function confirmPrompt()
+    {
+        var response = confirm("The questions could not be retrieved at this time, do you want to try again?");
+        if (response == true) {
+            getQuestions();
+        }
+    }
+    $scope.takeTestButtonTapped = function()
+    {
+        alert("it took the tap");
+        if($window.localStorage.questions === undefined)
+            getQuestions();
+        else {
+            $state.go('test.detail',{testID:1});
+        }
+    };
+    
+    if($window.localStorage.questions === undefined)
+        getQuestions();
+    else
+   TKQuestionsService.setQuestions(JSON.parse($window.localStorage.questions));
+}])
+.controller('TestCtrl', ['$scope', 'testInfo', '$stateParams', '$state',
+function($scope, testInfo, $stateParams, $state) {
+    //testInfo is passed in the router to obtain the questions
+    var qNumber = $stateParams.testID;
+    $scope.title = "Question #"+qNumber;
+    testInfo.forEach(function(infoDict)
+{
+        if(infoDict.Answer_ID === "A")
+            $scope.questionA = infoDict;
+        if(infoDict.Answer_ID === "B")
+            $scope.questionB = infoDict;
+        
+
+ });
+         $scope.buttonClicked = function ( option ) {
+                if(option === "A") {
+                    console.log("Chose A");
+                }
+                else if(option === "B") {
+                    console.log("Chose B");
+                }
+                var nextqNumber = Number(qNumber) +1;
+                if(nextqNumber>30) {
+                    $state.go('results');
+                }else {
+                    $state.go('test.detail',{testID:nextqNumber});
+                }
+            };
+
+}]);
+
